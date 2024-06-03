1c33746d9e7a17030abd42c1c291793547e5969c
friizor <mohamedzaidi768@gmail.com>
2024-06-03 20:14:44 +0100

    creating js function to get time

---
+++ main.js
@@ -0,0 +1,11 @@
+function displayTime() {
+    const date = new Date();
+    const hours = date.getHours().toString().padStart(2, "0");
+    const minutes = date.getMinutes().toString().padStart(2, "0");
+    const seconds = date.getSeconds().toString().padStart(2, "0");
+  
+    const time = `${hours}:${minutes}:${seconds}`;
+    hour.textContent = `${hours}:`;
+    minute.textContent = `${minutes}:`;
+    scn.textContent = `${seconds}`;
+  }
