67530cb7cd29d126f5fad8d1576cd9eeb7d3527d
friizor <mohamedzaidi768@gmail.com>
2024-06-03 21:48:50 +0100

    making js function to get date and display it

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ let hour = document.getElementById('hour');
 let minute = document.getElementById('mnt');
 let scn = document.getElementById('scn');
 
+let yearH = document.getElementById('year')
+let monthH = document.getElementById('month')
+let dayH = document.getElementById('day')
+
 
 
 function displayTime() {
@@ -9,11 +13,19 @@ function displayTime() {
     const hours = date.getHours().toString().padStart(2, "0");
     const minutes = date.getMinutes().toString().padStart(2, "0");
     const seconds = date.getSeconds().toString().padStart(2, "0");
+
+
+    const year = date.getFullYear();
+    const month = (date.getMonth()+ 1).toString().padStart(2, "0");;
+    const day = date.getDate().toString().padStart(2, "0");;
   
-    const time = `${hours}:${minutes}:${seconds}`;
     hour.textContent = `${hours}:`;
     minute.textContent = `${minutes}:`;
     scn.textContent = `${seconds}`;
+
+    yearH.innerHTML = `${year}-`;
+    monthH.innerHTML = `${month}-`;
+    dayH.innerHTML = `${day}`;
   }
   
   const intervalId = setInterval(displayTime, 1000);
